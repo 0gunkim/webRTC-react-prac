56f2154c0479223bd6e359b266eba198a059c6fa
Goldenprevue <goldenprevue@gmail.com>
2023-02-01 16:09:48 +0900

    [ADD] stream 추가 비디오 마이크 구현

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { useRef } from 'react';
+import styled from 'styled-components';
 import './App.css';
 
 function App() {
+  const videoRef = useRef();
+  const onClickGetVideo = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: false,
+    });
+    videoRef.current.srcObject = stream;
+    console.log(stream);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <StVideo
+        autoPlay
+        ref={videoRef}></StVideo>
+      <button onClick={onClickGetVideo}>버튼</button>
     </div>
   );
 }
+const StVideo = styled.video`
+  background-color: orange;
+  height: 100px;
+  width: 100px;
+`;
 
 export default App;
