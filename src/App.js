e4278f5fa6ad09c29af4fc4e3e33252be8af84e9
Goldenprevue <goldenprevue@gmail.com>
2023-02-11 00:03:09 +0900

    [ADD] 참가자 알림

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useRef, useState } from 'react';
+import { database } from './components/firebase';
 import pc from './components/webrtc';
+import { push, ref, getDatabase, onValue } from 'firebase/database';
 import styles from './app.module.css';
-export default function App() {
+import { uuidv4 } from '@firebase/util';
+export default function Aspp() {
   //localstream
   const localStreamRef = useRef();
   const [creatorUser, setCreatorUser] = useState();
@@ -13,18 +16,26 @@ export default function App() {
 
   //roomName
   const roomNameRef = useRef();
-  const [roomName, setRoomName] = useState('');
+  const [roomName, setRoomName] = useState();
+
+  //main && join
+  const [openVideo, setOpenVideo] = useState(false);
+
+  const locate = ref(database, `join`);
+  const offerLocate = ref(database, 'offer');
 
   const localStream = async () => {
     try {
-      const stream = await navigator.mediaDevices.getUserMedia(constraints);
-      localStreamRef.current.srcObject = stream;
-      setCreatorUser(stream);
+      if (openVideo) {
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        localStreamRef.current.srcObject = stream;
+        setCreatorUser(stream);
+      }
     } catch (error) {
       console.error('no connact localStream');
     }
   };
-
+  // video && audio off/on
   const videoSwitch = () => {
     const cameraTracks = creatorUser.getVideoTracks();
     cameraTracks.forEach((i) => {
@@ -43,40 +54,64 @@ export default function App() {
 
   const roomJoinSubmit = (e) => {
     e.preventDefault();
-    setRoomName(roomNameRef.current.value);
+    const inputValue = roomNameRef.current.value;
+    setRoomName(inputValue);
     roomNameRef.current.value = '';
+    setOpenVideo(!openVideo);
+    console.log(roomName);
+    offerHandler(inputValue);
   };
+
+  const offerHandler = async (inputValue) => {
+    const offer = await pc.createOffer();
+    const jsonOffer = JSON.stringify(offer);
+    push(offerLocate, jsonOffer);
+    push(locate, { roomName: inputValue });
+  };
+
+  // 참가자가 들어왔음 알리는 메세지
+  const welcomMessage = getDatabase();
+  const getJoin = ref(welcomMessage, `join`);
+  onValue(getJoin, (data) => {
+    const realFindRoom = Object.values(data.val());
+    const rd = realFindRoom.filter((i) => i.roomName === roomName);
+    rd.length === 2 && console.log('누군과 왔다.');
+  });
   useEffect(() => {
-    localStream();
-  }, []);
+    openVideo && localStream();
+  }, [openVideo]);
   return (
     <div className={styles.App}>
-      <form onSubmit={roomJoinSubmit}>
-        <div className={styles.roomNameBox}>
-          <label id='roomName'>
-            <h1>ROOM JOIN</h1>
-          </label>
-          <input
-            className={styles.roomNameInput}
-            id='roomName'
-            type='text'
-            ref={roomNameRef}
-          />
+      {!openVideo ? (
+        <form onSubmit={roomJoinSubmit}>
+          <div className={styles.roomNameBox}>
+            <label id='roomName'>
+              <h1>ROOM JOIN</h1>
+            </label>
+            <input
+              className={styles.roomNameInput}
+              id='roomName'
+              type='text'
+              ref={roomNameRef}
+            />
+            <button type='button'>JOIN</button>
+          </div>
+        </form>
+      ) : (
+        <div className={styles.vidoeBox}>
+          <video
+            className={styles.localStream}
+            ref={localStreamRef}
+            autoPlay
+            playsInline></video>
+          <button onClick={videoSwitch}>
+            {cameraOn ? 'CAMERA OFF' : 'CAMERA ON'}
+          </button>
+          <button onClick={audioSwitch}>
+            {audioOn ? 'AUDIO OFF' : 'AUDIO ON'}
+          </button>
         </div>
-      </form>
-      <div className={styles.vidoeBox}>
-        <video
-          className={styles.localStream}
-          ref={localStreamRef}
-          autoPlay
-          playsInline></video>
-        <button onClick={videoSwitch}>
-          {cameraOn ? 'CAMERA OFF' : 'CAMERA ON'}
-        </button>
-        <button onClick={audioSwitch}>
-          {audioOn ? 'AUDIO OFF' : 'AUDIO ON'}
-        </button>
-      </div>
+      )}
     </div>
   );
 }
