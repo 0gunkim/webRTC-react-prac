1ee3e8a8ed441bd92384851c3ad2bbae5e5feb6d
Goldenprevue <goldenprevue@gmail.com>
2023-02-14 21:32:56 +0900

    [ADD] candidate add

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { database, db } from './components/firebase';
+import { db } from './components/firebase';
 import pc from './components/webrtc';
 import styles from './app.module.css';
-import {
-  addDoc,
-  collection,
-  connectFirestoreEmulator,
-  doc,
-  onSnapshot,
-  query,
-  setDoc,
-} from 'firebase/firestore';
+import { addDoc, collection, onSnapshot, query } from 'firebase/firestore';
 import { nanoid } from 'nanoid';
 export default function Aspp() {
   //id
   const id = nanoid();
-  const [getid, setGetId] = useState('');
+  const [getid, setGetId] = useState(null);
   //localstream
   const localStreamRef = useRef();
   const [creatorUser, setCreatorUser] = useState();
@@ -30,10 +22,11 @@ export default function Aspp() {
   //roomName
   const roomNameRef = useRef();
   const [roomName, setRoomName] = useState();
+  const answernameRef = useRef();
 
   //main && join
-  const [openVideo, setOpenVideo] = useState(false);
-  const [welcome, setWelcom] = useState(false);
+  // const [openVideo, setOpenVideo] = useState(false);
+  // const [welcome, setWelcom] = useState(false);
   //offer && answer
   const [offer, setOffer] = useState();
   let inputValueFind;
@@ -41,31 +34,25 @@ export default function Aspp() {
   const localStream = async (inputValue) => {
     try {
       const stream = await navigator.mediaDevices.getUserMedia(constraints);
-      localStreamRef.current.srcObject = stream;
       setCreatorUser(stream);
 
       remoteStream = new MediaStream();
-      console.log(remoteStream);
-      yourStreamRef.current.srcObject = remoteStream;
 
       stream.getTracks().forEach((track) => {
-        console.log('track2', track);
         pc.addTrack(track, stream);
       });
       console.log('작동확인');
       pc.ontrack = (e) => {
-        console.log(e);
         e.streams[0].getTracks().forEach((track) => {
-          console.log('track3', track);
-
           remoteStream.addTrack(track);
         });
       };
 
+      localStreamRef.current.srcObject = stream;
+      yourStreamRef.current.srcObject = remoteStream;
       console.log(inputValue);
-
-      setOffer(offer.sdp);
     } catch (error) {
+      console.log(error);
       console.error('no connact localStream');
     }
   };
@@ -85,17 +72,32 @@ export default function Aspp() {
       setAudioOn(i.enabled);
     });
   };
-
   const roomJoinSubmit = async (e) => {
     e.preventDefault();
     const inputValue = roomNameRef.current.value;
     roomNameRef.current.value = '';
 
-    inputValueFind = getid && getid?.find((i) => i.inputValue === inputValue);
-    // console.log(inputValueFind);
-    if (!inputValueFind) {
-      setWelcom(true);
+    let cadidateAdd;
+    inputValueFind = getid?.filter((i) => i.inputValue);
+    const findRoom = inputValueFind?.find((i) => i.inputValue === inputValue);
+    console.log(inputValueFind);
+    if (!findRoom) {
       localStream(inputValue).then(async () => {
+        pc.onicecandidate = async (event) => {
+          console.log('캔디데이트 전송완료');
+          console.log(event);
+          pc.addIceCandidate(event.candidate);
+          // console.log(event.candidate.toJSON());
+          console.log(event);
+          cadidateAdd = event.candidate.toJSON();
+          // cadidateAdd = JSON.stringify(event.candidate);
+          console.log(cadidateAdd);
+          await addDoc(collection(db, `Room`), {
+            inputValue,
+            type: 'offerCandidate',
+            offerCadidate: cadidateAdd,
+          });
+        };
         const offer = await pc.createOffer();
         await addDoc(collection(db, `Room`), {
           sdp: offer.sdp,
@@ -108,44 +110,69 @@ export default function Aspp() {
       });
     } else {
       localStream(inputValue).then(() => {
+        console.log(inputValueFind);
         console.log('join');
+        const findOfferType = inputValueFind.find((i) => i.type === 'offer');
         const offerSession = new RTCSessionDescription({
-          sdp: inputValueFind.sdp,
-          type: inputValueFind.type,
+          sdp: findOfferType.sdp,
+          type: findOfferType.type,
         });
         pc.setRemoteDescription(offerSession).then(async () => {
           const answer = await pc.createAnswer();
-          console.log(answer);
           await pc.setLocalDescription(answer);
-          console.log(answer);
           await addDoc(collection(db, `Room`), {
             inputValue,
             sdp: answer.sdp,
             type: answer.type,
           });
         });
-        // remoteAnswer(inputValueFind);
+
         setAnswerRemote(inputValueFind);
-        console.log('remote 완료');
       });
+      setRoomName(inputValue);
+
+      const findTypeOfferCandy = inputValueFind.filter(
+        (i) => i.type === 'offerCandidate' && i.inputValue === inputValue
+      );
+      try {
+        pc.addIceCandidate(new RTCIceCandidate(findTypeOfferCandy));
+        console.log('연결성공');
+      } catch (e) {
+        console.log(e);
+      }
+
+      console.log(findTypeOfferCandy);
     }
-    setRoomName(inputValue);
   };
-
   const remoteAnswer = async () => {
-    console.log(answerRemote);
-    const answerSession = new RTCSessionDescription({
-      sdp: answerRemote.sdp,
-      type: answerRemote.type,
-    });
-    pc.setRemoteDescription(answerSession);
-    console.log('answer완료');
+    const getRoomName = answernameRef.current.value;
+    const findAnswer = getid.find((i) => i.inputValue === getRoomName);
+    if (findAnswer.type === 'answer') {
+      const answerSession = new RTCSessionDescription({
+        sdp: findAnswer.sdp,
+        type: findAnswer.type,
+      });
+      await pc.setRemoteDescription(answerSession);
+      console.log('answer완료');
+    }
   };
-
+  const candy = () => {
+    const q = query(collection(db, `Room`));
+    const unsub = onSnapshot(q, (query) => {
+      let arr = [];
+      query.forEach((doc) => {
+        arr.push(doc.data().inputValue);
+        console.log(arr);
+        const findOfferCandy = arr.filter((i) => i.inputValue === 'df');
+        console.log(findOfferCandy);
+      });
+    });
+  };
   useEffect(() => {
     const q = query(collection(db, `Room`));
     const unsub = onSnapshot(q, (query) => {
       let a = [];
+
       query.forEach((doc) => {
         a.push(doc.data());
         setGetId(a);
@@ -157,8 +184,8 @@ export default function Aspp() {
       unsub();
     };
   }, [roomName, offer, remoteStream, answerRemote]);
-  console.log(answerRemote);
-  console.log(welcome);
+
+  console.log(pc.curr);
   return (
     <div className={styles.App}>
       <form onSubmit={roomJoinSubmit}>
@@ -176,6 +203,11 @@ export default function Aspp() {
         </div>
       </form>
       <button onClick={remoteAnswer}> answer Remote</button>
+      <button onClick={candy}> candiateOffer</button>
+      <input
+        type='text'
+        ref={answernameRef}
+      />
       <div className={styles.vidoeBox}>
         <video
           className={styles.localStream}
